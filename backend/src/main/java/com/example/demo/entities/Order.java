3a3625de3792a3a6523f1de7aaf7075f938b449b
shaaqu <pawel.lukaszewicz99@gmail.com>
2020-04-23 00:28:42 +0200

    @OneToMany w Order, zrobine na jednokierunkowe

--- backend/src/main/java/com/example/demo/entities/Order.java
+++ backend/src/main/java/com/example/demo/entities/Order.java
@@ -16,8 +16,8 @@ public class Order {
     @ManyToOne(fetch = FetchType.LAZY) //domyslnie jest eager (ładujacy ze wszystkimi polami), a chcemy lazy
     private Account account;
 
-    @OneToMany(mappedBy = "product") //domyslnie jest lazy(spoczko) (ladujacy tylko na żądanie)
-    @Column(name = "products")
+    @JoinColumn(name = "product")
+    @OneToMany //domyslnie jest lazy(spoczko) (ladujacy tylko na żądanie)
     private Set<Product> product = new HashSet<Product>();
 
     public Order(){};
